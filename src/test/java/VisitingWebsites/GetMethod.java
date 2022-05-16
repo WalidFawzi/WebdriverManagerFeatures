c01b436bdd50db7e1eb63802e8700d49b77c6714
WalidFawzi <w.elbastawisi@diyarme.com>
2022-05-16 09:26:47 +0300

    Implicit / explicit waits Dealing with alerts examples

--- src/test/java/VisitingWebsites/GetMethod.java
+++ src/test/java/VisitingWebsites/GetMethod.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
@@ -30,12 +32,20 @@ public class GetMethod {
         WebElement source = driver.findElement(By.className(".source"));
         WebElement destination = driver.findElement(By.className(".dest"));
         action.dragAndDrop(source,destination);
+        //implicit wait >> wait until the action is done or return error
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //Explicit wait >> wait until the action is done and proceed with the other actions
+       // new WebDriverWait(driver,Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(driver.findElement(By.id("welcome"))));
         List <WebElement> courseList = driver.findElements(By.cssSelector(".course-list li"));
        System.out.println("The size is " + courseList.size());
         driver.findElement(By.id("inputEmail")).sendKeys("waleed@gmail.com");
         // Dropdwon with tag Select and has options
        Select coursesDropdown = new Select(driver.findElement(By.id("courses")));
        coursesDropdown.selectByIndex(2);
+       //Dealing with alerts
+      /*  driver.switchTo().alert().accept();
+        driver.switchTo().alert().getText();
+        driver.switchTo().alert().sendKeys("Waleed"); */
        driver.close();
 
 
